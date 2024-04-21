da162e10e52b2b9567a0cc0941fb18df553dfe6c
Yasoob Tahavi <yasoobtahavi54@gmail.com>
2024-04-21 05:10:02 +0500

    ArenaSearch Page
    not complete

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -4,110 +4,141 @@ import 'package:arenago/views/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:arenago/views/theme.dart';
 import 'package:arenago/views/TriggerMenu_ProfileButton.dart';
-
-
+import 'package:flutter/widgets.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
   _SearchPageState createState() => _SearchPageState();
-
 }
 
 class _SearchPageState extends State<SearchPage> {
   int _selectedIndex = 2;
 
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      
     });
     if (index == 4) {
-      Navigator.of(context).push(MaterialPageRoute
-                    (
-                      builder: (context) => const ProfileScreen(),
-                    ));
-    }
-    else if (index == 2) {
-                    Navigator.of(context).push(MaterialPageRoute
-                    (
-                      builder: (context) => const SearchPage(),
-                    ));
-    }
-    else if (index == 1) {
-                    Navigator.of(context).push(MaterialPageRoute
-                    (
-                      builder: (context) => const FriendsPage(),
-                    ));
-    }
-    else if (index == 0) {
-                    Navigator.of(context).push(MaterialPageRoute
-                    (
-                      builder: (context) => const HomePage(),
-                    ));
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const ProfileScreen(),
+      ));
+    } else if (index == 2) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const SearchPage(),
+      ));
+    } else if (index == 1) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const FriendsPage(),
+      ));
+    } else if (index == 0) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ));
     }
   }
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('ArenaGo'),
-    ),
-    body: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Card(
-            elevation: 10.0,
-            child: TextField(
-              decoration: const InputDecoration(
-                hintText: 'Search',
-                //contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                border: InputBorder.none,
-                prefixIcon: Icon(Icons.search),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ArenaGo'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Card(
+              elevation: 10.0,
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Search',
+                  //contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search),
+                ),
+                onChanged: (value) {
+                  // Perform search operation based on the input value
+                },
               ),
-              onChanged: (value) {
-                // Perform search operation based on the input value
-              },
             ),
           ),
-        ),
-      ],
-    ),
-    bottomNavigationBar: BottomNavigationBar(
-      backgroundColor: dBackgroundColor,
-      unselectedItemColor: loginOutlinecolor,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.people_alt),
-          label: 'Play Buddies',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.history_toggle_off),
-          label: 'History',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person_rounded),
-          label: 'Account',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: kPrimaryColor,
-      onTap: _onItemTapped,
-    ),
-  );
-}
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
 
-    
+            //width: double.infinity,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Full Name',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(200.0),
+                    borderSide: const BorderSide(color: loginOutlinecolor),
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+                  filled: true, // Set filled to true
+                  fillColor: Colors
+                      .white, // Set the color inside the text box to white
+                ),
+              ),
+              Row(
+                children: [],
+              ),
+              TextField(
+                style: new TextStyle(
+                    fontSize: 12.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+              TextField(
+                style: new TextStyle(
+                    fontSize: 12.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              )
+            ]),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: dBackgroundColor,
+        unselectedItemColor: loginOutlinecolor,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt),
+            label: 'Play Buddies',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history_toggle_off),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_rounded),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kPrimaryColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 }
